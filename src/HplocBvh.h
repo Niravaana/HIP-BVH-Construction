25f37406a4b0fcf68b9e5865a1bd93b411aa1a91
Paritosh <paritosh.kulkarni24@gmail.com>
2024-07-27 21:52:20 -0700

    Add initial code for hploc bvh build, morotn code calcualtion and sorting(need to add 64 bit morton code)
    Former-commit-id: 68ed33eebb8b830a078346a4c0ac25b18ab10b7d

---
+++ src/HplocBvh.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <src/Kernel.h>
+#include <src/Context.h>
+#include <src/Timer.h>
+#include <src/Common.h>
+#include <dependencies/Orochi/Orochi/OrochiUtils.h>
+#include <dependencies/Orochi/Orochi/GpuMemory.h>
+#include <vector>
+
+namespace BvhConstruction
+{
+	class HplocBvh
+	{
+	public:
+		void build(Context& context, std::vector<Triangle>& primitives);
+
+		void traverseBvh(Context& context);
+
+		Oro::GpuMemory<Triangle> d_triangleBuff;
+		Oro::GpuMemory<Aabb> d_triangleAabb;
+		Oro::GpuMemory<Aabb> d_sceneExtents;
+		Oro::GpuMemory<u32> d_mortonCodeKeys;
+		Oro::GpuMemory<u32> d_mortonCodeValues;
+		Oro::GpuMemory<u32> d_sortedMortonCodeKeys;
+		Oro::GpuMemory<u32> d_sortedMortonCodeValues;
+		u32 m_rootNodeIdx = 0;
+		Timer m_timer;
+		u32 m_nInternalNodes = 0;
+		float m_cost = 0.0f;
+	};
+}
