3a46343006c4c97479601d5243aaf0934f425ef2
Paritosh <paritosh.kulkarni24@gmail.com>
2024-07-31 18:43:50 -0700

    Init code for Ploc++ bvh builder
    Former-commit-id: aa21915c8fdfa0f26680d35d6ca98cf809e52770

---
+++ src/PLOC++Bvh.cpp
@@ -0,0 +1,103 @@
+#include "PLOC++Bvh.h"
+#include <src/Utility.h>
+#include <dependencies/stbi/stbi_image_write.h>
+#include <dependencies/stbi/stb_image.h>
+#include <ParallelPrimitives/RadixSort.h>
+#include <iostream>
+#include <queue>
+#include <assert.h>
+
+//#define WHILEWHILE 1
+#define IFIF 1
+#define _CPU 1
+using namespace BvhConstruction;
+
+void PLOC::build(Context& context, std::vector<Triangle>& primitives)
+{
+	const size_t primitiveCount = primitives.size();
+	d_triangleBuff.resize(primitiveCount); d_triangleBuff.reset();
+	d_triangleAabb.resize(primitiveCount); d_triangleAabb.reset(); //ToDo we might not need it.
+	OrochiUtils::copyHtoD(d_triangleBuff.ptr(), primitives.data(), primitives.size());
+	Oro::GpuMemory<int> d_parentIds(primitiveCount);
+	int val = -1;
+	OrochiUtils::memset(d_parentIds.ptr(), val, sizeof(int) * primitiveCount);
+
+	d_sceneExtents.resize(1); d_sceneExtents.reset();
+	{
+		Kernel centroidExtentsKernel;
+
+		buildKernelFromSrc(
+			centroidExtentsKernel,
+			context.m_orochiDevice,
+			"../src/CommonBlocksKernel.h",
+			"CalculateSceneExtents",
+			std::nullopt);
+
+		centroidExtentsKernel.setArgs({ d_triangleBuff.ptr(), d_triangleAabb.ptr(), d_sceneExtents.ptr(), primitiveCount });
+		m_timer.measure(TimerCodes::CalculateCentroidExtentsTime, [&]() { centroidExtentsKernel.launch(primitiveCount); });
+	}
+
+#if _DEBUG
+	const auto debugTriangle = d_triangleBuff.getData();
+	const auto debugAabb = d_triangleAabb.getData();
+	const auto debugExtent = d_sceneExtents.getData()[0];
+#endif
+
+	d_mortonCodeKeys.resize(primitiveCount); d_mortonCodeKeys.reset();
+	d_mortonCodeValues.resize(primitiveCount); d_mortonCodeValues.reset();
+	d_sortedMortonCodeKeys.resize(primitiveCount); d_sortedMortonCodeKeys.reset();
+	d_sortedMortonCodeValues.resize(primitiveCount); d_sortedMortonCodeValues.reset();
+	{
+		Kernel calulateMortonCodesKernel;
+
+		buildKernelFromSrc(
+			calulateMortonCodesKernel,
+			context.m_orochiDevice,
+			"../src/CommonBlocksKernel.h",
+			"CalculateMortonCodes",
+			std::nullopt);
+
+		calulateMortonCodesKernel.setArgs({ d_triangleAabb.ptr(), d_sceneExtents.ptr() , d_mortonCodeKeys.ptr(), d_mortonCodeValues.ptr(), primitiveCount });
+		m_timer.measure(TimerCodes::CalculateMortonCodesTime, [&]() { calulateMortonCodesKernel.launch(primitiveCount); });
+	}
+
+#if _DEBUG
+	const auto debugMortonCodes = d_mortonCodeKeys.getData();
+#endif
+	{
+		OrochiUtils oroUtils;
+		Oro::RadixSort sort(context.m_orochiDevice, oroUtils, 0, "../dependencies/Orochi/ParallelPrimitives/RadixSortKernels.h", "../dependencies/Orochi");
+
+		Oro::RadixSort::KeyValueSoA srcGpu{};
+		Oro::RadixSort::KeyValueSoA dstGpu{};
+		static constexpr auto startBit{ 0 };
+		static constexpr auto endBit{ 32 };
+		static constexpr auto stream = 0;
+
+		srcGpu.key = d_mortonCodeKeys.ptr();
+		srcGpu.value = d_mortonCodeValues.ptr();
+
+		dstGpu.key = d_sortedMortonCodeKeys.ptr();
+		dstGpu.value = d_sortedMortonCodeValues.ptr();
+
+		m_timer.measure(SortingTime, [&]() {
+			sort.sort(srcGpu, dstGpu, static_cast<int>(primitiveCount), startBit, endBit, stream); });
+	}
+
+#if _DEBUG
+	const auto debugSortedMortonCodes = d_sortedMortonCodeKeys.getData();
+	const auto debugSortedMortonCodesVal = d_sortedMortonCodeValues.getData();
+#endif
+
+	//We will need 
+	// B   - aabb array 
+	// MC  - sorted morton code array 
+	// I   - indices sorted based on the morton codes 
+	// PId - parents ids init to -1
+
+	//Build Hierarchy one kernel call will build the bvh
+}
+
+void PLOC::traverseBvh(Context& context)
+{
+}
