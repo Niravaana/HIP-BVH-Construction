2f597db3d79197c1abc29528e5970e506a2dea81
Nirvaana <paritosh.kulkarni24@gmail.com>
2024-08-08 11:42:26 -0400

    Remove do split from this file
    Former-commit-id: 8719242aaf1128373a82fe9d356713ad0a6e32a3

--- src/TwoPassLbvh.cpp
+++ src/TwoPassLbvh.cpp
@@ -26,91 +26,6 @@ T divideRoundUp(T value, U factor)
 	return (value + factor - 1) / factor;
 }
 
-static void doEarlySplitClipping(std::vector<Triangle>& inputPrims, std::vector<PrimRef>& outPrimRefs, float saMax = FltMax)
-{
-	std::queue<PrimRef> taskQueue;
-	for (int i = 0; i < inputPrims.size(); i++)
-	{
-		Aabb primAabb;
-		primAabb.grow(inputPrims[i].v1);
-		primAabb.grow(inputPrims[i].v2);
-		primAabb.grow(inputPrims[i].v3);
-		PrimRef ref = { i, primAabb };
-
-		taskQueue.push(ref);
-	}
-
-	while (!taskQueue.empty())
-	{
-		PrimRef ref = taskQueue.front(); taskQueue.pop();
-
-		if (ref.m_aabb.area() <= saMax)
-		{
-			outPrimRefs.push_back(ref);
-		}
-		else
-		{
-			const auto extent = ref.m_aabb.extent();
-			const int dim = ref.m_aabb.maximumExtentDim();
-			const auto centre = ref.m_aabb.center();
-			const auto offset = ref.m_aabb.m_min + (extent - centre);
-
-			float3 lMin = ref.m_aabb.m_min;
-			float3 lMax;
-			if (dim == 0)
-			{
-				lMax.x = centre.x;
-				lMax.y = ref.m_aabb.m_max.y;
-				lMax.z = ref.m_aabb.m_max.z;
-			}
-			if (dim == 1)
-			{
-				lMax.x = ref.m_aabb.m_max.x; 
-				lMax.y = centre.y;
-				lMax.z = ref.m_aabb.m_max.z;
-			}
-			if (dim == 2)
-			{
-				lMax.x = ref.m_aabb.m_max.x;
-				lMax.y = ref.m_aabb.m_max.y;
-				lMax.z = centre.z; 
-			}
-
-			Aabb L = { lMin, lMax };
-
-			float3 RMin;
-			float3 RMax = ref.m_aabb.m_max;
-			if (dim == 0)
-			{
-				RMin.x = centre.x;
-				RMin.y = ref.m_aabb.m_min.y;
-				RMin.z = ref.m_aabb.m_min.z;
-			}
-			if (dim == 1)
-			{
-				RMin.x = ref.m_aabb.m_min.x;
-				RMin.y = centre.y; 
-				RMin.z = ref.m_aabb.m_min.z;
-			}
-			if (dim == 2)
-			{
-				RMin.x = ref.m_aabb.m_min.x;
-				RMin.y = ref.m_aabb.m_min.y;
-				RMin.z = centre.z; 
-			}
-
-			Aabb R = { RMin, RMax };
-
-			PrimRef LRef = { ref.m_primIdx, L };
-			PrimRef RRef = { ref.m_primIdx, R };
-
-			taskQueue.push(LRef);
-			taskQueue.push(RRef);
-		}
-	}
-}
-
-
 void TwoPassLbvh::build(Context& context, std::vector<Triangle>& primitives)
 {
 	d_triangleBuff.resize(primitives.size()); d_triangleBuff.reset();
@@ -119,9 +34,9 @@ void TwoPassLbvh::build(Context& context, std::vector<Triangle>& primitives)
 	std::vector<PrimRef> h_primRefs;
 #ifdef USE_PRIM_SPLITTING
 	float saMax = 10.0f;
-	doEarlySplitClipping(primitives, h_primRefs, saMax);
+	Utility::doEarlySplitClipping(primitives, h_primRefs, saMax);
 #else
-	doEarlySplitClipping(primitives, h_primRefs);
+	Utility::doEarlySplitClipping(primitives, h_primRefs);
 #endif
 
 	const size_t primitiveCount = h_primRefs.size();
