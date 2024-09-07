7c13d6d8e72ea18596b4f211d6ccaa03ed0adbc0
Nirvaana <paritosh.kulkarni24@gmail.com>
2024-09-06 21:05:55 -0700

    Update Ploc++Kernel.h

--- src/Ploc++Kernel.h
+++ src/Ploc++Kernel.h
@@ -192,6 +192,10 @@ extern "C" __global__ void Ploc(int* nodeIndices0, int* nodeIndices1, Bvh2Node* 
 	int gIdx = blockIdx.x * blockDim.x + threadIdx.x;
 	int blockOffset = blockDim.x * blockIdx.x;
 
+	/*if PlocRadius is 2 then we need to read 4(2 * plocRadius elements to the right and 2 * plocRadius at the end of block size, the original paper refers to as chunk.
+	  Because of this neighbour research limited to chunk we dont need separate global prefix sum pass as original ploc method. This is the reason we can combine 
+	  Neighbour search, merge and compact pass in one kernel.*/
+
 	alignas(alignof(Aabb)) __shared__ u8 aabbSharedMem[sizeof(Aabb) * (PlocBlockSize + 4 * PlocRadius)];
 	__shared__ u64 neighbourIndicesSharedMem[PlocBlockSize + 4 * PlocRadius];//block size of neighbouring node Indices from search of nodeIndices0
 	__shared__ int nodeIndicesSharedMem[PlocBlockSize + 4 * PlocRadius]; //block size of node Indices from nodeIndices0
@@ -201,6 +205,9 @@ extern "C" __global__ void Ploc(int* nodeIndices0, int* nodeIndices1, Bvh2Node* 
 	u64* ptrNeighbourIndices  = neighbourIndicesSharedMem + 2 * PlocRadius;
 	int* ptrNodeIndices = nodeIndicesSharedMem + 2 * PlocRadius;
 
+	/*distance between i, j will be calculated in right search and similarly j, i will be calculated in left search which will be same
+	  So distance is commutative. Hence we encode both in same loop by constructing 64 bit key and high 32 bits will hold area and lower 32 bits will hold i or j index.
+	  Finally we store minAreaAndIndex in ptrNeighbourIndices[tId] from which we can decode back index*/
 	for (int neighbourIdx = int(threadIdx.x) - 2 * PlocRadius; neighbourIdx < int(blockDim.x) + 2 * PlocRadius; neighbourIdx += blockDim.x)
 	{
 		int clusterIdx = neighbourIdx + blockOffset; 
@@ -252,17 +259,34 @@ extern "C" __global__ void Ploc(int* nodeIndices0, int* nodeIndices1, Bvh2Node* 
 		int neighbourIdx = (ptrNeighbourIndices[currentClusterIdx] & 0xffffffff) - blockOffset;
 		int rightChildIdx = ptrNodeIndices[neighbourIdx];
 		int neighboursNeighbourIDx = (ptrNeighbourIndices[neighbourIdx] & 0xffffffff) - blockOffset;
-		
+
+		/* Now we will check conditions on merge
+		   current cluster = threadIdx.x should be equal to neighboursNeighbourIDx i,e cluster neighbour pairs we found should be mutual.
+		   For threads having smaller cluster index will only will merge ie, t1 dealing with pair (2,4) and t6 with (4,2) then t1 will do
+		   the merge copy new node at cluster index 2 and t6 will mark cluster index 4 invalid */
+
 		bool merge = false;
 		if (currentClusterIdx == neighboursNeighbourIDx)
 		{
+			/*(Point A) For our example t1 and t6 will come here but t6 wont go in if so do nothing
+			So for t6 the nodeIdx will be invalid value*/
 			if (currentClusterIdx < neighbourIdx) merge = true;
 		}
 		else
 		{
+			//Rest of the threads will just copy cluster index they are processing as is
 			nodeIdx = leftChildIdx;
 		}
-		
+
+		/*
+		  Every thread will have merge valur to 1 or 0. So if we have 6 threads with following merge values 
+		  [ 0 1 0 1 1 0 0] -> implies 3 threads will merge and will have new nodes.
+		  So we can efficiently calculate binary prefix sum using binaryWarpPrefixSum.
+		  For a given thread it will return prefix sum value which is new node offset for that thread.
+		  in above example for thread t3 binaryWarpPrefixSum will return 2 and nMergedClusters will return 
+		  3 which is total merged nodes this block produced. This value will be useful to calculate new nClusters 
+		  after this kernel call is done.
+		*/
 		int mergedNodeIdx = nClusters - 2 - binaryWarpPrefixSum(merge, nMergedClusters);
 		if (merge)
 		{
@@ -277,19 +301,35 @@ extern "C" __global__ void Ploc(int* nodeIndices0, int* nodeIndices1, Bvh2Node* 
 	}
 
 	__syncthreads();
+	/*
+	  Now we need to do compaction pass if you notice (Point A)
+	  After merging pass for all the threads that had found valid neighbour cluster pairs but did not merge the nodeIdx will have invalid value.
+	  In compaction phase we want to shift all the invalid values at the end of nodeIndices array.
+	  We calculate nodes that dont have invalid idx. Again we use binary prefix sum on condition nodeIdx != invalid idx but for the block.
+	*/
 
 	int newblockOffset = binaryBlockPrefixSum(nodeIdx != INVALID_NODE_IDX, nodeIndicesSharedMem);
-	
+
+	/*
+	  We want to find new positions(cluster Indices) in nodeIndices array where we want to copy merged node and the nodes which are still remaining to be processed(which did not found any neighbours yet).
+	  Both of these nodeIdx will have not invalid value set to them. So in given block we need to calculate how many much nodes are there.
+	  Though blockOffset is for current block say block0,  we need to add it to blockOffset of block1 then blockOffset of block0 + block1 to block2 so on. 
+	  We do this below using one thread in block to calculate blockOffset and it waits on the atomic counter that is set by the previous block thread.
+	*/
+
 	if (threadIdx.x == blockDim.x - 1)
 	{
 		
-		while (atomicAdd(atomicBlockCounter, 0) < blockIdx.x);
-		localBlockOffset = atomicAdd(blockOffsetSum, newblockOffset);
+		while (atomicAdd(atomicBlockCounter, 0) < blockIdx.x);//wait for prev block thread to increament counter
+		localBlockOffset = atomicAdd(blockOffsetSum, newblockOffset);// If we are block3 then get sum of blockOffset0 + blockOffset1 + blockOffset2
 		atomicAdd(atomicBlockCounter, 1);
 	}
 
 	__syncthreads();
 
+	/*
+	  Put merged nodeIdx into the new cluster positions we calculated above by compaction.
+	*/
 	if (gIdx < nClusters)
 	{
 		if (nodeIdx != INVALID_NODE_IDX)
